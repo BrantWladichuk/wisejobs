f39bd12d60c54fd0397cd4730801211da7da4a00
Brant Wladichuk <brantwladichuk@gmail.com>
2025-07-30 09:21:07 -0700

    Secure dashboard routes with Admin middleware

--- routes/settings.php
+++ routes/settings.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Middleware\Admin;
 
-Route::middleware('auth')->group(function () {
+Route::middleware(Admin::class)->group(function () {
     Route::redirect('settings', '/settings/profile');
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\PostController;
+use App\Http\Middleware\Admin;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(Admin::class)->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
+require __DIR__.'/companies.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
